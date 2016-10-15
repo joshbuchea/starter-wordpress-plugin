5c5aa3b4021ebdef9f02c01e9ae0505200c40eed
Josh Buchea <verctrl@joshbuchea.com>
2016-10-14 23:11:15 -0700

    Simplify plugin further

--- plugin-name.php
+++ plugin-name.php
@@ -1,30 +1,23 @@
 <?php
 /**
- * Plugin Name:     Starter WordPress Plugin
+ * Plugin Name:     Plugin Name
  * Plugin URI:      http://example.com/plugin-name-uri/
  * Description:     This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:         1.0.0
  * Author:          Your Name or Your Company
  * Author URI:      http://example.com/
- * Text Domain:     starter-wordpress-plugin
+ * Text Domain:     plugin-name
  * Domain Path:     /languages
  */
 
-// If this file is called directly, abort.
-if ( ! defined( 'WPINC' ) ) die;
+if ( !defined('ABSPATH') ) exit; // Exit if accessed directly
 
-/**
- * The code that runs during plugin activation.
- */
-register_activation_hook( __FILE__, 'activate_plugin_name' );
-function activate_plugin_name() {
-	// do stuff here...
-}
+if ( !function_exists('starter_wordpress_plugin') ) {
 
-/**
- * The code that runs during plugin deactivation.
- */
-register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
-function deactivate_plugin_name() {
-	// do stuff here...
+  function starter_wordpress_plugin() {
+    // Hello world!
+  }
+
+  starter_wordpress_plugin();
+
 }
